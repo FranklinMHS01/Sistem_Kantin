ade0de122a11fce0e2aedcb112e3cad2620697f3
FranklinMHS01 <franklinchang0129@gmail.com>
2023-10-23 09:38:40 +0700

    Frist Commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php 
+
+$databaseHost = 'localhost';
+$databaseName = 'crud_db';
+$databaseUsername = 'root';
+$databasePassword = 'root1001';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername , $databasePassword, $databaseName);
+?>
