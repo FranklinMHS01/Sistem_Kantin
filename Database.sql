c26a85341ebbb34f3d3dcabb7c36398da4e3ca03
FranklinMHS01 <franklinchang0129@gmail.com>
2023-10-23 09:51:51 +0700

    First Commit

--- Database.sql
+++ Database.sql
@@ -1,11 +1,11 @@
 create database crud_db;
 
-use crud_db;
+use `crud_db`;
 
-CREATE TABLE 'users' (
-    'id' int(11) not null auto_increment,
-    'name' varchar(100),
-    'email' varchar(100),
-    'mobile' varchar(100),
-    PRIMARY KEY ('id')
+CREATE TABLE `users` (
+    `id` int(11) not null auto_increment,
+    `name` varchar(100),
+    `email` varchar(100),
+    `mobile` varchar(15),
+    PRIMARY KEY (`id`)
 );
